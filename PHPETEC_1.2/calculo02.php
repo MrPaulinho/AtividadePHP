6c6c20f323c3895b8bec2f50a9fb151485d9bfaf
PauloMachado <paulo-machado95@hotmail.com>
2021-08-27 17:42:21 -0300

    atualizacao

--- PHPETEC_1.2/calculo02.php
+++ PHPETEC_1.2/calculo02.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <main>
+        <div class="mensagem1">
+            <?php
 
     /*variaveis*/
 
@@ -13,11 +23,29 @@
         $num08 = $_POST ["num08"];
         $num09 = $_POST ["num09"];
         $num10 = $_POST ["num10"];
+        $contp = ("contp");
+        $contn = ("contn");
+
+        /*lista*/
+        $lista = array($num01 , $num02, $num03, $num04, $num05,
+        $num06, $num07, $num08, $num09, $num10);
+
+        /*média*/
+        $media = array_sum($lista) / 10;
+        echo "a média entre a soma dos valores informados é {$media}.";
+
+        /*qtd nº negativo*/
+
+        /*qtd nº positivo*/
+
+
+
 
     }
 
-
-
-
-
 ?>
+        </div>
+        <a href="index.php"><button>Voltar</button></a>
+    </main>
+</body>
+</html>
