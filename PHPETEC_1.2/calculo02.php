44075b2a8e38b84a1fbc2a3abe53cf5258a401b4
PauloMachado <paulo-machado95@hotmail.com>
2021-08-27 18:07:53 -0300

    atualizado

--- PHPETEC_1.2/calculo02.php
+++ PHPETEC_1.2/calculo02.php
@@ -23,7 +23,8 @@
         $num08 = $_POST ["num08"];
         $num09 = $_POST ["num09"];
         $num10 = $_POST ["num10"];
-
+        $Positivos = 0;
+        $Negativos = 0;
 
         /*lista*/
         $lista = array($num01 , $num02, $num03, $num04, $num05,
@@ -31,12 +32,58 @@
 
         /*média*/
         $media = array_sum($lista) / 10;
-        echo "a média entre a soma dos valores informados é {$media}.";
+        echo "a média entre a soma dos valores informados é {$media}.</br>";
 
-        /*qtd nº negativo*/
+        /*qtd nº positivo e negativos*/
+        if($num01 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num02 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num03 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num04 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num05 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num06 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num07 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num08 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num09 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }if($num10 >= 0){
+            $Positivos = $Positivos + 1;
+        }else{
+            $Negativos = $Negativos + 1;
+        }
 
-        /*qtd nº positivo*/
+        echo "Quantidade de Numero positivos {$Positivos} Quantidade de Numeros negativos {$Negativos}";
 
+        $Positivos = (100 * $Positivos) / 10;
+        $Negativos = (100 * $Negativos) / 10;
+
+        echo '<br> Percentual de numeros positivos ' . $Positivos .'%' .',
+        Percentual de numeros negativos ' . $Negativos.'%';
 
     }
 
