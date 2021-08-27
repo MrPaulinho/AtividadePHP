348115bd65893aa5adee26927ac57f1fb92d8349
PauloMachado <paulo-machado95@hotmail.com>
2021-08-27 17:42:56 -0300

    at

--- PHPETEC_1.2/calculo02.php
+++ PHPETEC_1.2/calculo02.php
@@ -23,8 +23,7 @@
         $num08 = $_POST ["num08"];
         $num09 = $_POST ["num09"];
         $num10 = $_POST ["num10"];
-        $contp = ("contp");
-        $contn = ("contn");
+
 
         /*lista*/
         $lista = array($num01 , $num02, $num03, $num04, $num05,
@@ -37,8 +36,6 @@
         /*qtd nº negativo*/
 
         /*qtd nº positivo*/
-
-
 
 
     }
