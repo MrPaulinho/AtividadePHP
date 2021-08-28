eb69a4379e1e2c70dda12fbb585959637bb06725
PauloMachado <paulo-machado95@hotmail.com>
2021-08-27 22:35:35 -0300

    atualizações

--- PHPETEC_1.3/calculo01.php
+++ PHPETEC_1.3/calculo01.php
@@ -4,9 +4,47 @@
 
 if ($_POST) {
     $num01 = $_POST ["num01"];
-        for($i = 0; $i <= 10; $i++){
-        echo "{$num01} x {$i} = ".($num01*$i). "<br>";
-        }
+    $num02 = $_POST ["num02"];
+    $num03 = $_POST ["num03"];
+    $num04 = $_POST ["num04"];
+    $num05 = $_POST ["num05"];
+    $num06 = $_POST ["num06"];
+    $num07 = $_POST ["num07"];
+    $num08 = $_POST ["num08"];
+    $num09 = $_POST ["num09"];
+    $num10 = $_POST ["num10"];
+    $genero1 = $_POST ["genero1"];
+    $genero2 = $_POST ["genero2"];
+    $genero3 = $_POST ["genero3"];
+    $genero4 = $_POST ["genero4"];
+    $genero5 = $_POST ["genero5"];
+    $genero6 = $_POST ["genero6"];
+    $genero7 = $_POST ["genero7"];
+    $genero8 = $_POST ["genero8"];
+    $genero9 = $_POST ["genero9"];
+    $genero10 = $_POST ["genero10"];
+
+    /*maior altura*/
+    $lista = array($num01, $num02, $num03, $num04, $num05, $num06, $num07, $num08, $num09, $num10);
+    $maior = max($lista);
+
+    /*maior é homem ou mulher*/
+    $listas = array(
+    array($num01 => "num01",$genero1 => "genero1"),
+    array($num02 => "num02",$genero2 => "genero2"),
+    array($num03 => "num03",$genero3 => "genero3"),
+    array($num04 => "num04",$genero4 => "genero4"),
+    array($num05 => "num05",$genero5 => "genero5"),
+    array($num06 => "num06",$genero6 => "genero6"),
+    array($num07 => "num07",$genero7 => "genero7"),
+    array($num08 => "num08",$genero8 => "genero8"),
+    array($num09 => "num09",$genero9 => "genero9"),
+    array($num10 => "num10",$genero10 => "genero10"),
+    );
+
+    foreach($listas as $result){
+
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -18,6 +56,11 @@ if ($_POST) {
 </head>
 <body>
     <main>
+        <div class="resultado">
+            <?php
+            echo "a maior altura é {$maior}.";
+            ?>
+        </div>
         <a href="index.php"><button>Voltar</button></a>
     </main>
 </body>
--- PHPETEC_1.3/funcoes.php
+++ PHPETEC_1.3/funcoes.php
@@ -12,13 +12,104 @@
         <body>
             <main>
                 <div class="exercicio">
-                    <h2>Crie um programa que entre com os dados de altura e sexo de 10 pessoas.</h2><br>
+                    <h2>Crie um programa que entre com os dados de altura e sexo de 10 pessoas.</h2><br/>
                         -Imprima na tela quantas pessoas são do sexo masculino e quantas pessoas são do sexo feminino.<br>
-                        -Mostre também qual é a maior altura<br>
-                        -E se essa altura é de um homem ou uma mulher<br>
+                        -Mostre também qual é a maior altura<br/>
+                        -E se essa altura é de um homem ou uma mulher<br/>
                 </div>
+                <br/>
                 <form action="calculo01.php" method = "POST">
-
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num01" step = "0.01" required/><br/>
+                        <select checkbox="genero1" name = "genero1" required>
+                            <option name = "genero1" value="H">Homem</option>
+                            <option name = "genero1" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num02" step = "0.01" required/><br/>
+                        <select checkbox="genero2" name = "genero2" required>
+                            <option name = "genero2" value="H">Homem</option>
+                            <option name = "genero2" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num03" step = "0.01" required/><br/>
+                        <select checkbox="genero3" name = "genero3" required>
+                            <option name = "genero3" value="H">Homem</option>
+                            <option name = "genero3" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num04" step = "0.01" required/><br/>
+                        <select checkbox="genero4" name = "genero4" required>
+                            <option name = "genero4" value="H">Homem</option>
+                            <option name = "genero4" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num05" step = "0.01" required/><br/>
+                        <select checkbox="genero5" name = "genero5" required>
+                            <option name = "genero5" value="H">Homem</option>
+                            <option name = "genero5" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num06" step = "0.01" required/><br/>
+                        <select checkbox="genero6" name = "genero6" required>
+                            <option name = "genero6" value="H">Homem</option>
+                            <option name = "genero6" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num07" step = "0.01" required/><br/>
+                        <select checkbox="genero7" name = "genero7" required>
+                            <option name = "genero7" value="H">Homem</option>
+                            <option name = "genero7" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num08" step = "0.01" required/><br/>
+                        <select checkbox="genero8" name = "genero8" required>
+                            <option name = "genero8" value="H">Homem</option>
+                            <option name = "genero8" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num09" step = "0.01" required/><br/>
+                        <select checkbox="genero9" name = "genero9" required>
+                            <option name = "genero9" value="H">Homem</option>
+                            <option name = "genero9" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <div class="menu">
+                        <br/>
+                        Altura:  <input type="number" name="num10" step = "0.01" required/><br/>
+                        <select checkbox="genero10" name = "genero10" required>
+                            <option name = "genero10" value="H">Homem</option>
+                            <option name = "genero10" value="M">Mulher</option>
+                        </select>
+                    </div>
+                    <br/>
+                    <button class="botao" type="submit">Enviar</button>
                 </form>
             </main>
         </body>
